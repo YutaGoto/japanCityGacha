d40839f99eea36fd7792e9beddab7abc7a6b19b2
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2025-01-04 01:12:17 +0900

    :sparkles: add city stock

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import {
+  Box,
+  Button,
+  ColorSchemeProvider,
+  Flex,
+  Heading,
+  IconButton,
+  IconButtonLink,
+  SelectList,
+  Text,
+  TextField,
+} from "gestalt";
+
 import { cities } from "./cities";
 import { prefectures } from "./prefectures";
-import "./App.css";
 import { useState } from "react";
+import { ColorScheme } from "gestalt/dist/contexts/ColorSchemeProvider";
+
+interface City {
+  type: string;
+  code: string;
+  name: string;
+  kana: string;
+  city_code: string;
+  city_name: string;
+  city_kana: string;
+  pref_code: string;
+  pref_name: string;
+  pref_kana: string;
+}
 
 function App() {
-  const [pickedCity, setPickedCity] = useState({
-    city_name: "",
-    city_code: "",
-  });
+  const [scheme, setScheme] = useState<ColorScheme>("userPreference");
+
+  const [pickedCity, setPickedCity] = useState<City | undefined>(undefined);
+  const [pickedCities, setPickedCities] = useState<City[]>([]);
+
   const [pickedPrefecture, setPickedPrefecture] = useState({
-    pref_name: "",
-    pref_code: "",
+    pref_name: "北海道",
+    pref_code: "010006",
   });
 
-  const pickPrefecture = () => {
-    setPickedCity({ city_name: "", city_code: "" });
-    const randomIndex = Math.floor(Math.random() * prefectures.length);
-    setPickedPrefecture(prefectures[randomIndex]);
+  const selectPrefecture = (prefectureCode: string) => {
+    const prefecture = prefectures.find(
+      (prefecture) => prefecture.pref_code === prefectureCode
+    );
+    if (!prefecture) {
+      return;
+    }
+    setPickedCity(undefined);
+    setPickedPrefecture(prefecture);
   };
 
   const pickCity = () => {
@@ -27,55 +57,76 @@ function App() {
     });
     const randomIndex = Math.floor(Math.random() * prefectureCity.length);
     setPickedCity(prefectureCity[randomIndex]);
+    setPickedCities([...pickedCities, prefectureCity[randomIndex]]);
   };
 
   return (
-    <>
-      <div className="github">
-        <a
-          href="https://github.com/YutaGoto/japanCityGacha"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>
-      </div>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div>
-        <h1>市区町村ルーレット</h1>
-        <div className="buttons">
-          <button className="button" onClick={pickPrefecture}>
-            都道府県
-          </button>
-          <button className="button" onClick={pickCity}>
-            市町村
-          </button>
-        </div>
-        {pickedPrefecture.pref_name && (
-          <div>
-            <h3>
-              🎉 {pickedPrefecture.pref_name}
-              {pickedCity.city_name && pickedCity.city_name} 🎉
-            </h3>
-          </div>
-        )}
-      </div>
-      <div>
-        <h2>候補: {cities.length}</h2>
-        <ul>
-          {cities.map((city) => (
-            <li key={city.code}>{city.name}</li>
-          ))}
-        </ul>
-      </div>
-    </>
+    <Flex alignContent="center" justifyContent="center">
+      <ColorSchemeProvider colorScheme={scheme}>
+        <Box color="default" minHeight="100vh" minWidth="100vw" paddingX={4}>
+          <Box
+            role="navigation"
+            direction="row"
+            display="flex"
+            justifyContent="end"
+            paddingX={4}
+            paddingY={3}
+          >
+            <IconButton
+              accessibilityLabel="Toggle color scheme"
+              icon={scheme === "light" ? "moon" : "sun"}
+              onClick={() => setScheme(scheme === "light" ? "dark" : "light")}
+            />
+            <IconButtonLink
+              accessibilityLabel="GitHub"
+              href="https://github.com/YutaGoto/japanCityGacha"
+              target="blank"
+              rel="nofollow"
+              icon="visit"
+            />
+          </Box>
+          <Flex justifyContent="center">
+            <Box>
+              <Heading accessibilityLevel={1}>市区町村ルーレット</Heading>
+              <Flex alignItems="end" gap={4}>
+                <SelectList
+                  label="都道府県"
+                  id="prefecture"
+                  value={pickedPrefecture.pref_code}
+                  onChange={(e) => selectPrefecture(e.value)}
+                >
+                  {prefectures.map((prefecture) => (
+                    <SelectList.Option
+                      key={prefecture.pref_code}
+                      value={prefecture.pref_code}
+                      label={prefecture.pref_name}
+                    />
+                  ))}
+                </SelectList>
+                <Button text="市町村ルーレット" onClick={pickCity} />
+              </Flex>
+              <Box marginTop={4}>
+                {pickedCity?.city_name && (
+                  <TextField
+                    id="city"
+                    name="city"
+                    value={`${pickedPrefecture.pref_name}${pickedCity.city_name}`}
+                    onChange={() => {}}
+                  />
+                )}
+              </Box>
+              <Box>
+                <Heading accessibilityLevel={2}>選ばれた市町村</Heading>
+
+                {pickedCities.map((city) => (
+                  <Text key={city.city_code}>{city.name}</Text>
+                ))}
+              </Box>
+            </Box>
+          </Flex>
+        </Box>
+      </ColorSchemeProvider>
+    </Flex>
   );
 }
 
