91e61cc112454d10bd00c30fe45442fd39d9d1d6
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2025-05-06 17:36:30 +0900

    :recycle: separate components

---
+++ src/components/PickedCities.tsx
@@ -0,0 +1,33 @@
+import { Box, Heading, Button, Text } from "gestalt";
+import { City } from "../types";
+
+interface PickedCitiesProps {
+  pickedCities: City[];
+  setPickedCity: (city: City | undefined) => void;
+  setPickedCities: (cities: City[]) => void;
+}
+
+export const PickedCities = ({ pickedCities, setPickedCity, setPickedCities }: PickedCitiesProps) => {
+  return (
+    <Box>
+      <Box>
+        <Heading accessibilityLevel={2}>選ばれた市町村</Heading>
+
+        {pickedCities.map((city) => (
+          <Text key={city.city_code}>{city.name}</Text>
+        ))}
+      </Box>
+
+      <Box marginTop={2}>
+        <Button
+          text="リセット"
+          color="red"
+          onClick={() => {
+            setPickedCity(undefined);
+            setPickedCities([]);
+          }}
+        />
+      </Box>
+    </Box>
+  );
+};
