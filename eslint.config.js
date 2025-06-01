3929378f8446713941d627b6da69a18bc0417be8
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2025-06-01 21:23:02 +0900

    :heavy_plus_sign: add ESLint Plugin Config

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,16 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import { builtinCommands } from 'eslint-plugin-command/commands'
+import command from 'eslint-plugin-command/config'
 
 export default tseslint.config(
   { ignores: ['dist'] },
+  command({
+    commands: [
+      ...builtinCommands,
+    ]
+  }),
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
